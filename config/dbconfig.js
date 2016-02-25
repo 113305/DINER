598ceead1a28049378ffbde182426bc45852b095
MINSEON <113305@naver.com>
2016-02-25 18:04:37 +0900

    add dbconfig.js

---
+++ config/dbconfig.js
@@ -0,0 +1,11 @@
+var config = {
+    "host": process.env.DINER_DB_SERVER,
+    "port": process.env.DINER_DB_PORT,
+    "user": process.env.DINER_DB_USERNAME,
+    "password": process.env.DINER_DB_PASSWORD,
+    "database": process.env.DINER_DB,
+    "ssl": "Amazon RDS",
+    "debug": true
+}
+
+module.exports = config;
