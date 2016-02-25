a7fc2250dc5ffa4399a93e1900e8d3c249ac9edd
MINSEON <113305@naver.com>
2016-02-25 14:48:19 +0900

    finish facebook login

--- routes/auth.js
+++ routes/auth.js
@@ -4,20 +4,8 @@ var async = require('async');
 var passport = require('passport');
 var router = express.Router();
 
-router.post('/login', function(req, res, next) {
-    //if (req.secure) {
-    //    var result = {
-    //        "results": {
-    //            "message": "로그인이 정상적으로 처리되었습니다."
-    //        }
-    //    };
-    //    res.json(result);
-    //} else {
-    //    var err = new Error('SSL/TLS Ugrades Required');
-    //    err.status = 426;
-    //    next(err);
-    //}
 
+router.post('/login', function(req, res, next) {
     if (req.secure) {
         passport.authenticate('local-login', function(err, customer, info) {
             if(err) {
@@ -49,21 +37,35 @@ router.post('/login', function(req, res, next) {
     }
 });
 
-//router.post('/facebook/token', function(req, res, next) {
-//    if (req.secure) {
-//        var result = {
-//            "results": {
-//                "message": "페이스북 토큰 수신이 정상적으로 처리되었습니다."
-//            }
-//        };
-//        res.json(result);
-//    } else {
-//        var err = new Error('SSL/TLS Ugrades Required');
-//        err.status = 426;
-//        next(err);
-//    }
-//});
-//
+router.post('/facebook/token', function(req, res, next) {
+    if (req.secure) {
+        passport.authenticate('facebook-token', function(err, customer, info) {
+            if (err) {
+                var err = new Error('페이스북 토큰 수신에 실패하였습니다.');
+                err.code = 'E0002';
+                next(err);
+            } else {
+                req.logIn(customer, function(err) {
+                    if (err) {
+                        next(err);
+                    } else {
+                        var result = {
+                          "results": {
+                              "message": "페이스북 토큰 수신이 정상적으로 처리되었습니다."
+                          }
+                        };
+                        req.json(result);
+                    }
+                });
+            }
+        });
+    } else {
+        var err = new Err('SSL/TLS Upgrades Required');
+        err.status = 426;
+        next(err);
+    }
+});
+
 //router.post('/updatepassword', function(req, res, next) {
 //    if (req.secure) {
 //        var result = {
