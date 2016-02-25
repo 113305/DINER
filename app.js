d968a6616f8641ecf473187fa1954f07709d862e
MINSEON <113305@naver.com>
2016-02-25 18:31:09 +0900

    app.js

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
-var regions = require('./routes/regions');
-var app = express();
+var passport = require('passport');
+var session = require('express-session');
 
 global.pool = require('./config/dbpool');
+require('./config/passportconfig')(passport);
+
+var auth = require('./routes/auth');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +24,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    "secret": "pd11jonIrlQL1eBvv4MBF8pvugEV21PnYGTiaUaAcrM=",
+    "cookie": { "maxage" : "31536000000" },
+    "resave": true,
+    "saveUninitialized": true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/regions', regions);
 
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,11 +52,10 @@ if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({
-      "error": {
-        "code": err.code
-      }
-      message: err.message,
-      error: err
+        "error": {
+            "code": err.code,
+            "message": err.message
+        }
     });
   });
 }
@@ -59,6 +69,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
 
 
 module.exports = app;
