c9f9488ae7abfb0be3923be13aac72a20412a64e
MINSEON <113305@naver.com>
2016-02-26 01:10:35 +0900

    start admin

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+//TODO: cron써서 스케줄링 하기
+//TODO: 푸시하기
+router.post('/reservationpushs/notifystate', function(req, res, next) {
+
+});
+
+router.post('/reservationpushs/notifytime', function(req, res, next) {
+
+});
+
+module.exports = router;
