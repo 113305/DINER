c7e52af02e9558e4e63a1013897d4219f4407649
SUNMI <kitty0704@naver.com>
2016-03-02 12:25:02 +0900

    회원가입하기 테스트

--- routes/customers.js
+++ routes/customers.js
@@ -35,7 +35,7 @@ router.post('/', function (req, res, next) {
         }
 
         function selectCustomer(connection, callback) {
-            var sql = "SELECT id " +
+            var sql = "SELECT customer_id " +
                 "FROM customer " +
                 "WHERE email = aes_encrypt(" + connection.escape(email) + ", unhex(" + connection.escape(hexkey) + "))";
             connection.query(sql, function (err, results) {
@@ -84,7 +84,7 @@ router.post('/', function (req, res, next) {
                 "VALUES (aes_encrypt(" + connection.escape(email) + ", unhex(" + connection.escape(hexkey) + ")), " +
                 "        aes_encrypt(" + connection.escape(name) + ", unhex(" + connection.escape(hexkey) + ")), " +
                 "        aes_encrypt(" + connection.escape(phone) + ", unhex(" + connection.escape(hexkey) + ")), " +
-                connection.escape(hashPassword) + ")";
+                         connection.escape(hashPassword) + ")";
 
             connection.query(sql1, function (err, result) {
                 connection.release();
@@ -129,16 +129,16 @@ router.delete('/', isLoggedIn, function (req, res, next) {
             }
         });
     }
-
     function deleteCustomer(connection, callback) {
         var sql2 = "DELETE " +
             "FROM customer " +
-            "WHERE id = ?";
+            "WHERE customer_id = ?";
         connection.query(sql2, [req.session.id], function (err, result) {
             connection.release();
             if (err) {
                 callback(err);
             } else {
+                console.log('세션아이디',req.session.id);
                 callback(null);
             }
         });
@@ -177,10 +177,23 @@ router.get('/me', isLoggedIn, function (req, res, next) {  // 내 정보 요청
     }
 
     function getCustomer(connection, callback) {  //페북으로 가입한사람이랑 로컬가입한사람이랑 어떻게 보여주지? 
-        var sql = "SELECT aes_decrypt(customer_name), unhex(" + connection.escape(hexkey) + ")), " +// 복호화해와야함 
-            "FROM customer " +  // 학교가서 제대로 가져오기 
-            "WHERE customer_id = ?";
-            
+
+        if (!req.session.facebookName === null)  //페이스북회원
+        {
+
+        } else { // 로컬회원
+
+        }
+        //var sql = "SELECT convert(aes_decrypt(email, unhex('62cd950982ee48dfc4352a516c599ccb5ee3e88ddaf4adb04081a5acf09aa3b0')) using utf8),
+        //convert(aes_decrypt(customer_name, unhex('62cd950982ee48dfc4352a516c599ccb5ee3e88ddaf4adb04081a5acf09aa3b0')) using utf8),
+        //convert(aes_decrypt(customer_phone, unhex('62cd950982ee48dfc4352a516c599ccb5ee3e88ddaf4adb04081a5acf09aa3b0')) using utf8),
+        //show_count,
+        //    convert(aes_decrypt(facebook_email, unhex('62cd950982ee48dfc4352a516c599ccb5ee3e88ddaf4adb04081a5acf09aa3b0')) using utf8),
+        //convert(aes_decrypt(facebook_name, unhex('62cd950982ee48dfc4352a516c599ccb5ee3e88ddaf4adb04081a5acf09aa3b0')) using utf8)
+        //FROM customer
+        //WHERE customer_id = 42;
+
+
         connection.query(sql, [req.session.id], function (err, result) {
             if (err) {
                 callback(err);
