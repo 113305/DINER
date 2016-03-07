d1c90da49e9a0462b1192aa6b8b364162abe23d7
MINSEON <113305@naver.com>
2016-03-07 19:26:00 +0900

    add job

--- routes/admin.js
+++ routes/admin.js
@@ -26,7 +26,10 @@ router.get('/:reservationId', function(req, res, next) {
     function selectReservationState(connection, callback) {
         var select = "SELECT registration_token, restaurant_name, date_time, before_35m, before_60m, reservation_state " +
                      "FROM reservation JOIN customer " +
-                     "                 ON (reservation.customer_id = customer.customer_id)";
+                     "                 ON (reservation.customer_id = customer.customer_id)" +
+                     "                 JOIN restaurant " +
+                     "                 ON (reservation.restaurant_id = restaurant.restaurant_id)" +
+                     "WHERE reservation_id = ?";
 
         connection.query(select, [reservationId], function(err, results) {
             if (err) {
@@ -52,36 +55,52 @@ router.get('/:reservationId', function(req, res, next) {
         message.addNotification("title", "DINER");
 
         //예약 상태가 대기일경우
-        if (reservationState === 0) {
-            //job 생성하기
-            function createStatePushJob(outercb) {
-                var m = moment().tz('Asia/Seoul');
-                var date = m.toDate();
-                var jobName = "job-" + uuid.v4();
-                var job = nodeschedule.scheduleJob(jobName, date, function(mm) {
+        if (pushInfo.reservationState === 0) {
+            async.eachSeries([pushInfo.before60, pushInfo.before35], function(beforeTime, cb) {
+                var info = '';
+                var time = '';
+                if (beforeTime === pushInfo.before60) {
+                    info = '60분 전 입니다. 감사합니다.';
+                    time = 'B60';
+                } else {
+                    info = '35분 전 입니다. 예약 취소가 불가능 합니다.';
+                    time = 'B35';
+                }
+                var jobName = time + uuid.v4();
+                var job = nodeschedule.scheduleJob(jobName, beforeTime, function() {
+                    pool.getConnection(function(err, connection) {
 
-                    var content = pushInfo.dateTime + pushInfo.restaurantName + ' 예약 신청이 완료되었습니다. 감사합니다.';
-                    message.addNotification("body", content);
+                        var content = pushInfo.dateTime + pushInfo.restaurantName + ' 예약 시간 ' + info;
+                        message.addNotification("body", content);
 
-                    sender.send(message, pushInfo.registrationToken, function(err, result) {
-                        if (err) {
-                            outercb(err);
-                        }  else {
-                            var update = "UPDATE job " +
-                                         "SET result_log = ? " +
-                                         "WHERE job_name = ?";
+                        var select = "SELECT job_id " +
+                            "FROM job " +
+                            "WHERE job_name = ?";
 
-                            connection.query(update, [result, jobName], function(err, result) {
+                        connection.query(select, [jobName], function(err, results) {
+                            var jobId = results[0].job_id;
+                            sender.send(message, pushInfo.registrationToken, function(err, result) {
                                 if (err) {
-                                    outercb(err);
-                                } else {
-                                    outercb(null);
+                                    var insert = "INSERT INTO result_log (content, job_id) " +
+                                        "VALUES (?, ?)";
+
+                                    connection.query(insert, [err, jobId], function(err, result) {
+                                        connection.release();
+                                        console.log(jobName + ' error result_log 처리 완료');
+                                    });
+                                }  else {
+                                    var insert = "INSERT INTO result_log (content, job_id) " +
+                                        "VALUES (?, ?)";
+
+                                    connection.query(insert, [result, jobId], function(err, result) {
+                                        connection.release();
+                                        console.log(jobName + ' success result_log 처리 완료');
+                                    });
                                 }
                             });
-                        }
+                        });
                     });
-
-                }.bind(null, m));
+                });
 
                 //jobdb에 넣어주기
                 var insert = "INSERT INTO job (job_name, reservation_id) " +
@@ -89,75 +108,35 @@ router.get('/:reservationId', function(req, res, next) {
 
                 connection.query(insert, [jobName, reservationId], function(err, result) {
                     if (err) {
-                        outercb(err);
+                        callback(err);
                     } else {
-                        outercb(null);
-                    }
-                });
-            }
+                        var jobId = result.insertId;
+                        var content = "create job";
 
-            function createBeforePushJob(outercb) {
-                async.each([pushInfo.before60, pushInfo.before35], function(beforeTime, innercb) {
-                    var date = beforeTime;
-                    if (beforeTime === pushInfo.before60) {
-                        var infoTime = '60분전 입니다. 감사합니다.';
-                    } else {
-                        var infoTime = '35분전 입니다. 예약 취소가 불가능 합니다.';
-                    }
-                    var jobName = "job-" + uuid.v4();
-                    var job = nodeschedule.scheduleJob(jobName, date, function(mm) {
+                        var insert = "INSERT INTO result_log (content, job_id) " +
+                            "VALUES (?, ?)";
 
-                        var content = pushInfo.dateTime + pushInfo.restaurantName + ' 예약 시간 ' + infoTime;
-                        message.addNotification("body", content);
-
-                        sender.send(message, pushInfo.registrationToken, function(err, result) {
+                        connection.query(insert, [content, jobId], function(err) {
                             if (err) {
-                                cb(err);
-                            }  else {
-                                var update = "UPDATE job " +
-                                             "SET result_log = ? " +
-                                             "WHERE job_name = ?";
-
-                                connection.query(update, [result, jobName], function(err, result) {
-                                    if (err) {
-                                        cb(err);
-                                    } else {
-                                        cb(null);
-                                    }
-                                });
+                                console.log(jobName + ' add result_log 처리 에러');
+                            } else {
+                                console.log(jobName + ' add result_log 처리 완료');
                             }
+
                         });
-
-                    }.bind(null, m));
-
-                    //jobdb에 넣어주기
-                    var insert = "INSERT INTO job (job_name, reservation_id) " +
-                        "VALUES (?, ?)";
-
-                    connection.query(insert, [jobName, reservationId], function(err, result) {
-                        if (err) {
-                            cb(err);
-                        } else {
-                            cb(null);
-                        }
-                    });
-                }, function(err) {
-                    outercb(err);
+                        cb(null);
+                    }
                 });
-            }
 
-            async.parallel([
-                createStatePushJob,
-                createBeforePushJob
-            ], function (err, results) {
-                if(err){
-                    callback(err);
-                }else {
-                    callback(null);
-                }
+            }, function(err) {
+                callback(err);
             });
+            connection.release();
+            console.log('wwwwwwwwwwwwwwwwwwwwwwwwww');
 
-        } else {//예약 상태가 변경이나 취소일 경우
+        } else {
+
+            //예약 상태가 변경이나 취소일 경우
             //예약아이디로 jobName 알아내기
             var select = "SELECT job_name " +
                          "FROM job " +
@@ -172,106 +151,152 @@ router.get('/:reservationId', function(req, res, next) {
                         var selectedJob = nodeschedule.scheduledJobs[jobName];
                         selectedJob.cancel();
                         delete nodeschedule.scheduledJobs[jobName];
+
+                        var select = "SELECT job_id " +
+                            "FROM job " +
+                            "WHERE job_name = ?";
+
+                        connection.query(select, [jobName], function(err, results) {
+                            var jobId = results[0].job_id;
+                            var result = 'canceled job';
+
+                            var insert = "INSERT INTO result_log (content, job_id) " +
+                                "VALUES (?, ?)";
+
+                            connection.query(insert, [result, jobId], function(err, result) {
+                                console.log(jobName + ' cancel result_log 처리 완료');
+                            });
+                        });
+
                         cb(null);
                     }, function(err) {
+                        connection.release();
                         callback(err);
                     });
                 }
             });
 
-            if (reservationState === 4) {//예약 상태가 변경일 경우
+            if (pushInfo.reservationState === 4) {//예약 상태가 변경일 경우
                 //job 생성하기
                 function createBeforePushJob(outercb) {
                     async.each([pushInfo.before60, pushInfo.before35], function(beforeTime, innercb) {
-                        var date = beforeTime;
+                        var info = '';
+                        var time = '';
                         if (beforeTime === pushInfo.before60) {
-                            var infoTime = '60분전 입니다. 감사합니다.';
+                            info = '60분 전 입니다. 감사합니다.';
+                            time = 'B60';
                         } else {
-                            var infoTime = '35분전 입니다. 예약 취소가 불가능 합니다.';
+                            info = '35분 전 입니다. 예약 취소가 불가능 합니다.';
+                            time = 'B35';
                         }
-                        var jobName = "job-" + uuid.v4();
-                        var job = nodeschedule.scheduleJob(jobName, date, function(mm) {
+                        var jobName = time + uuid.v4();
+                        var job = nodeschedule.scheduleJob(jobName, beforeTime, function() {
+                            pool.getConnection(function(err, connection) {
 
-                            var content = pushInfo.dateTime + pushInfo.restaurantName + ' 예약 시간 ' + infoTime;
-                            message.addNotification("body", content);
+                                var content = pushInfo.dateTime + pushInfo.restaurantName + ' 예약 시간 ' + info;
+                                message.addNotification("body", content);
 
-                            sender.send(message, pushInfo.registrationToken, function(err, result) {
-                                if (err) {
-                                    cb(err);
-                                }  else {
-                                    var update = "UPDATE job " +
-                                        "SET result_log = ? " +
-                                        "WHERE job_name = ?";
+                                var select = "SELECT job_id " +
+                                    "FROM job " +
+                                    "WHERE job_name = ?";
 
-                                    connection.query(update, [result, jobName], function(err, result) {
+                                connection.query(select, [jobName], function(err, results) {
+                                    var jobId = results[0].job_id;
+                                    sender.send(message, pushInfo.registrationToken, function(err, result) {
                                         if (err) {
-                                            cb(err);
-                                        } else {
-                                            cb(null);
+                                            var insert = "INSERT INTO result_log (content, job_id) " +
+                                                "VALUES (?, ?)";
+
+                                            connection.query(insert, [err, jobId], function(err, result) {
+                                                connection.release();
+                                                console.log(jobName + ' error result_log 처리 완료');
+                                            });
+                                        }  else {
+                                            var insert = "INSERT INTO result_log (content, job_id) " +
+                                                "VALUES (?, ?)";
+
+                                            connection.query(insert, [result, jobId], function(err, result) {
+                                                connection.release();
+                                                console.log(jobName + ' success result_log 처리 완료');
+                                            });
                                         }
                                     });
-                                }
+                                });
                             });
-
-                        }.bind(null, m));
+                        });
 
                         //jobdb에 넣어주기
                         var insert = "INSERT INTO job (job_name, reservation_id) " +
-                                     "VALUES (?, ?)";
+                            "VALUES (?, ?)";
 
                         connection.query(insert, [jobName, reservationId], function(err, result) {
                             if (err) {
-                                cb(err);
+                                connection.release();
+                                innercb(err);
                             } else {
-                                cb(null);
+                                var select = "SELECT job_id " +
+                                    "FROM job " +
+                                    "WHERE job_name = ?";
+
+                                connection.query(select, [jobName], function(err, results) {
+                                    var jobId = results[0].job_id;
+
+                                    var insert = "INSERT INTO result_log (content, job_id) " +
+                                        "VALUES (?, ?)";
+
+                                    connection.query(insert, [result, jobId], function(err, result) {
+                                        console.log(jobName + ' add result_log 처리 완료');
+                                    });
+                                });
+                                innercb(null);
                             }
                         });
                     }, function(err) {
                         outercb(err);
                     });
                 }
+
                 //예약상태 변경에서 대기로 변경하기
                 function updateReservationState(outercb) {
                     var update = "UPDATE reservation " +
                                  "SET reservation_state = 0 " +
                                  "WHERE reservation_id = ?";
                     connection.query(update, [reservationId], function(err, result) {
-                       if (err) {
-                           cb(err);
-                       } else {
-                           cb(null);
-                       }
+                        connection.release();
+                        if (err) {
+                           outercb(err);
+                        } else {
+                           outercb(null);
+                        }
                     });
                 }
 
                 async.parallel([
                     createBeforePushJob,
                     updateReservationState
-                ], function (err, results) {
-                    if(err){
-                        callback(err);
-                    }else {
-                        callback(null);
-                    }
+                ], function (err) {
+                    callback(err);
                 });
-
-
             }
         }
+        callback(null);
     }
 
-    async.waterfall([getConnection, selectReservationState, createJob], function(err) {
+    async.waterfall([getConnection, selectReservationState, createJob], function(err, results) {
+
         if (err) {
-            var err = new Error('');
+            var err = new Error('ㅋㅋㅋㅋㅋㅋㅋㅋㅋ');
             err.code = '';
             next(err);
         } else {
             var results = {
                 "result": {
-                    "message": ""
+                    "message": "ddddddddd"
                 }
             };
-            next(results);
+
+
+            res.json(results);
         }
     });
 });
