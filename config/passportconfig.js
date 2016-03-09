4b4e4e6acfe3b23b3005f7c8997cff3ab454e799
MINSEON <113305@naver.com>
2016-03-09 16:55:17 +0900

    finish facebook-token

--- config/passportconfig.js
+++ config/passportconfig.js
@@ -20,7 +20,6 @@ var passportconfig = function(passport){
                              "convert(aes_decrypt(customer_name, unhex(" + connection.escape(hexkey) + ")) using utf8) as name, " +
                              "convert(aes_decrypt(email, unhex(" + connection.escape(hexkey) + ")) using utf8) as email, " +
                              "convert(aes_decrypt(customer_phone, unhex(" + connection.escape(hexkey) + ")) using utf8) as phone, " +
-                             "convert(aes_decrypt(facebook_email, unhex(" + connection.escape(hexkey) + ")) using utf8) as facebookEmail, " +
                              "convert(aes_decrypt(facebook_name, unhex(" + connection.escape(hexkey) + ")) using utf8) as facebookName " +
                              "FROM customer " +
                              "WHERE customer_id = " + connection.escape(id);
@@ -127,82 +126,84 @@ var passportconfig = function(passport){
     passport.use('facebook-token', new FacebookTokenStrategy({
         "clientID": authConfig.facebook.appId,
         "clientSecret": authConfig.facebook.appSecret,
-        "profileFields": ["id", "displayName", "emails"]
-    }, function (accessToken, refreshToken, profile, done) {
-
-        function getConnection(callback) {
-            pool.getConnection(function(err, connection) {
-                if (err) {
-                    callback(err);
-                } else {
-                    callback(null, connection);
-                }
-            });
-        }
-
-        function selectOrCreateCustomer(connection, callback) {
-            var select = "SELECT customer_id, facebook_token " +
-                         "FROM customer " +
-                         "WHERE facebook_id = ?";
-            connection.query(select, [profile.id], function(err, results) {
-                if (err) {
-                    connection.release();
-                    callback(err);
-                } else {
-                    if (results.length === 0 ) {
-
-                        var insert = "INSERT INTO customer (facebook_id, facebook_token, facebook_email, facebook_name) " +
-                                     "VALUES(" + connection.escape(profile.id) + ", " +
-                                                 connection.escape(accessToken) + ", " +
-                                                 "aes_encrypt(" + connection.escape(profile.emails[0]) + ", unhex(" + connection.escape(hexkey) + "))" +  ", " +
-                                                 "aes_encrypt(" + connection.escape(profile.displayName) + ", unhex(" + connection.escape(hexkey) + "))" +
-                                            ")";
-                        connection.query(insert, function(err, result) {
-                            connection.release();
-                            if (err) {
-                                callback(err);
-                            } else {
-                                var customer = {
-                                    "id": result.insertId
-                                };
-                                callback(null, customer);
-                            }
-                        });
+        "profileFields": ["id", "displayName"]
+        }, function (accessToken, refreshToken, profile, done) {
+            console.log(profile);
+            function getConnection(callback) {
+                pool.getConnection(function(err, connection) {
+                    if (err) {
+                        callback(err);
                     } else {
-                        if (accessToken === results[0].facebook_token) {
-                            connection.release();
-                            var customer = {
-                                "id": results[0].customer_id
-                            };
-                            callback(null, customer);
-                        } else {
-                            var update = "UPDATE customer " +
-                                         "SET	facebook_token = ? " +
-                                         "WHERE facebook_id = ?";
-                            connection.query(update, [accessToken, profile.id], function(err, result) {
+                        callback(null, connection);
+                    }
+                });
+            }
+
+            function selectOrCreateCustomer(connection, callback) {
+                var select = "SELECT customer_id, facebook_token " +
+                             "FROM customer " +
+                             "WHERE facebook_id = ?";
+                connection.query(select, [profile.id], function(err, results) {
+
+
+                    if (err) {
+                        connection.release();
+                        var err = new Error('1');
+                        callback(err);
+                    } else {
+                        if (results.length === 0 ) {
+
+                            var insert = "INSERT INTO customer (facebook_id, facebook_token, facebook_name) " +
+                                         "VALUES(" + connection.escape(profile.id) + ", " +
+                                                     connection.escape(accessToken) + ", " +
+                                                     "aes_encrypt(" + connection.escape(profile.displayName) + ", unhex(" + connection.escape(hexkey) + "))" +
+                                                ")";
+                            connection.query(insert, function(err, result) {
                                 connection.release();
                                 if (err) {
                                     callback(err);
                                 } else {
                                     var customer = {
-                                        "id": results[0].customer_id
+                                        "id": result.insertId
                                     };
                                     callback(null, customer);
                                 }
                             });
+                        } else {
+                            if (accessToken === results[0].facebook_token) {
+                                connection.release();
+                                var customer = {
+                                    "id": results[0].customer_id
+                                };
+                                callback(null, customer);
+                            } else {
+                                var update = "UPDATE customer " +
+                                             "SET	facebook_token = ? " +
+                                             "WHERE facebook_id = ?";
+                                connection.query(update, [accessToken, profile.id], function(err, result) {
+                                    connection.release();
+                                    if (err) {
+                                        callback(err);
+                                    } else {
+                                        var customer = {
+                                            "id": results[0].customer_id
+                                        };
+                                        callback(null, customer);
+                                    }
+                                });
+                            }
                         }
                     }
+                });
+            }
+
+            async.waterfall([getConnection, selectOrCreateCustomer], function(err, customer) {
+                if (err) {
+                    done(err);
+                } else {
+                    done(null, customer);
                 }
             });
-        }
-
-        async.waterfall([getConnection, selectOrCreateCustomer], function(err, customer) {
-            if (err) {
-                done(err);
-            } else {
-                done(null, customer);
-            }
-        });
     }));
 };
 
