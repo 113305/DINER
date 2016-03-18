f362e94e7cee8405230ec0a359b96a6bff27e81e
MINSEON <113305@naver.com>
2016-03-18 16:20:43 +0900

    add err logger

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var loggerMorgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
+var logger = require('./config/loggerconfig');
 
 global.pool = require('./config/dbpool');
 require('./config/passportconfig')(passport);
@@ -25,7 +26,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(loggerMorgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -60,20 +61,22 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        "error": {
-            "code": err.code,
-            "message": err.message
-        }
+    app.use(function(err, req, res, next) {
+        logger.log('error', err);
+        res.status(err.status || 500);
+        res.json({
+            "error": {
+                "code": err.code,
+                "message": err.message
+            }
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+    logger.log('error', err);
     res.status(err.status || 500);
     res.json({
         "error": {
